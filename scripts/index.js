9ddde6a9d6485943f2ed0d630e524919162e4cda
the717q <the717q@gmail.com>
2023-02-08 05:02:42 +0000

    version1
    - updates
    - updates on several pages
    - content created for 2 main pages

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+import { links } from "../data/links.js";
+
+
+const linksSection = document.querySelector(".section-links");
+const articleContainer = document.querySelector(".article-container");
+
+const elements = links.map((links) => {
+  const keys = Object.keys(links);
+  return `<div class="links"><p class=${keys[0]}>${links.website}</p>
+    <a href=${links.url} class=${keys[1]} target="_blank"">${links.userName} </a></div>`
+});
+
+console.log(linksSection)
+const date = `<p>Website Hosted at ${window.location.hostname}<br>Local Path: ${window.location.href}</p>`
+
+
+linksSection.insertAdjacentHTML("beforeend", elements.join(" "));
+
+articleContainer.insertAdjacentHTML('afterend', date)
